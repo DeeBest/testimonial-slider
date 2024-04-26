1cb5e5b4d08e7887d7910b69ac07426318bc272f
DeeBest <simphiwedladla8@gmail.com>
2024-04-26 20:50:32 +0200

    applied basic styling

---
+++ index.js
@@ -0,0 +1,3 @@
+const footerParagraph = document.getElementById('footer-paragraph');
+const currentDate = new Date().getFullYear();
+footerParagraph.textContent = `Created by \xA9Simphiwe ${currentDate}.`;
